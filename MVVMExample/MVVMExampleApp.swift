0206a902518298b81196bbf3e726bcd2997f7630
Isaac Iniongun <iniongunisaac@gmail.com>
2023-04-19 20:21:29 +0100

    Initial Commit

---
+++ MVVMExample/MVVMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMExampleApp.swift
+//  MVVMExample
+//
+//  Created by Isaac Iniongun on 19/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MVVMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
